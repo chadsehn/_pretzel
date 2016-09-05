adc0124903756ac54bdd20344094f1acf2327dfe
Chad Sehn <chad@baytek.ca>
2016-09-05 16:05:49 -0400

    Tweak spacing of _nx( call since it seems to be breaking the Travis CI build even though the args seem correct.

--- comments.php
+++ comments.php
@@ -27,8 +27,16 @@ if ( post_password_required() ) {
 	if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
-				printf( // WPCS: XSS OK.
-					esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', '_pretzel' ) ),
+				// WPCS: XSS OK.
+				// https://codex.wordpress.org/Function_Reference/_nx
+				printf( 
+					esc_html( _nx( 
+						'One thought on &ldquo;%2$s&rdquo;', 
+						'%1$s thoughts on &ldquo;%2$s&rdquo;', 
+						get_comments_number(), 
+						'comments title', 
+						'_pretzel' 
+					) ),
 					number_format_i18n( get_comments_number() ),
 					'<span>' . get_the_title() . '</span>'
 				);
