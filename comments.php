ac64882fa9e9c50da17e271cec1eb3acb6b44c9d
Chad Sehn <chad@baytek.ca>
2016-09-05 16:10:10 -0400

    removing whitespace from end of lines.

--- comments.php
+++ comments.php
@@ -27,15 +27,17 @@ if ( post_password_required() ) {
 	if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
-				// WPCS: XSS OK.
-				// https://codex.wordpress.org/Function_Reference/_nx
-				printf( 
-					esc_html( _nx( 
-						'One thought on &ldquo;%2$s&rdquo;', 
-						'%1$s thoughts on &ldquo;%2$s&rdquo;', 
-						get_comments_number(), 
-						'comments title', 
-						'_pretzel' 
+				/**
+				 * WPCS: XSS OK.
+				 * https://codex.wordpress.org/Function_Reference/_nx
+				 */
+				printf(
+					esc_html( _nx(
+						'One thought on &ldquo;%2$s&rdquo;',
+						'%1$s thoughts on &ldquo;%2$s&rdquo;',
+						get_comments_number(),
+						'comments title',
+						'_pretzel'
 					) ),
 					number_format_i18n( get_comments_number() ),
 					'<span>' . get_the_title() . '</span>'
