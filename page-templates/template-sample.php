da4307acd08baeb549f356db6eb21aed097d57df
Chad Sehn <chad@baytek.ca>
2016-09-05 16:53:39 -0400

    Adding sample page template file for illustrating file organization and naming conventions.

---
+++ page-templates/template-sample.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Template Name: Sample Layout
+ * 
+ * This is a sample page template for doing a different layout.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package _pretzel
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
